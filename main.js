f657fac5cd7e24c68f7061116e349be0f7691a1c
vinirlq7 <145574909+vinirlq7@users.noreply.github.com>
2025-09-30 12:15:45 -0300

    30/09

---
+++ main.js
@@ -0,0 +1,72 @@
+
+
+
+
+
+const perguntas = [
+    {
+        enunciado: "Pergunta?",
+        alternativas: [
+            {
+                texto: " Texto/resposta",
+                afirmacao: " Afirmação ",
+            },
+            {
+                texto: " Texto/resposta ",
+                afirmacao: " Afirmação ",
+            }
+        ]
+    },
+    {
+        enunciado: " Pergunta?",
+        alternativas: [
+            {
+                texto: " Texto/resposta ",
+                afirmacao: " Afirmação ",
+            },
+            {
+                texto: " Texto/resposta ",
+                afirmacao: " Afirmação ",
+            }
+        ]
+    },
+    {
+        enunciado: " Pergunta?",
+        alternativas: [
+            {
+                texto: " Texto/resposta ",
+                afirmacao: " Afirmação ",
+            },
+            {
+                texto: " Texto/resposta ",
+                afirmacao: " Afirmação ",
+            }
+        ]
+    },
+    {
+        enunciado: " Pergunta?",
+        alternativas: [
+            {
+                texto: " Texto/resposta ",
+                afirmacao: " Afirmação ",
+            },
+            {
+                texto: " Texto/resposta ",
+                afirmacao: " Afirmação ",
+            }
+        ]
+    },
+    {
+        enunciado: " Pergunta?",
+        alternativas: [
+            {
+                texto: " Texto/resposta ",
+                afirmacao: "Afirmação",
+            },
+            {
+                texto: " Texto/resposta ",
+                afirmacao: " Afirmação ",
+            }
+        ]
+    },
+];
