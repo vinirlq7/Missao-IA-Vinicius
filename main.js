f836a6956334894298d6280e843fe8d48ea975a6
vinirlq7 <145574909+vinirlq7@users.noreply.github.com>
2025-10-21 11:31:14 -0300

    21/10

--- main.js
+++ main.js
@@ -79,6 +79,19 @@ let perguntaAtual;
 function mostraPergunta(){
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
+    mostraAlternativas();
 }
+function mostraAlternativas(){
+    for (const alternativa of perguntaAtual.alternativas){
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click", function(){
+            atual++
+            mostraPergunta();
+        })
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
+}
+
 
 mostraPergunta();
